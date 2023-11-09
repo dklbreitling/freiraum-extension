b31a49004de3df1455bcc3bd83fba8709f07517e
David Breitling <dklbreitling@gmail.com>
2023-11-09 09:48:21 +0100

    Scroll into view.

--- loadMenu.js
+++ loadMenu.js
@@ -5,11 +5,20 @@ fetch(url /*, options */)
         const html = new DOMParser().parseFromString(text, "text/html");
         const content = document.getElementById("content");
         content.innerHTML = html.querySelector("#menuplanThisWeek table").innerHTML;
+        document.getElementById("title").innerHTML = html.querySelector(".freiraumText .container-fluid.container-xxl").innerHTML;
         content.querySelector("button").style.display = "none";
-        const day = new Date().getDay() - 1; // 0 is Sunday, minus 1 to index
+        const day = 3;//new Date().getDay() - 1; // 0 is Sunday, minus 1 to index
         if (day >= 0 && day < 5) {
-            /* should be weekday */
-            content.querySelectorAll(".td_day")[day].classList.add("today");
+            /* Only if today is a weekday. */
+            todayEl = content.querySelectorAll(".td_day")[day];
+            todayEl.classList.add("today");
+            const rect = todayEl.getBoundingClientRect();
+            /* Only scroll if not visible. */
+            if (!(rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)))
+                todayEl.scrollIntoView({ behavior: "instant", block: "end", inline: "end" });
         }
     })
     .catch((error) => {
