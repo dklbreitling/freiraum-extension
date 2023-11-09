725d613defd1128a4c62ad4e4ceac4316275805e
David Breitling <dklbreitling@gmail.com>
2023-11-09 09:20:32 +0100

    removed unnecessary scripts and styles

--- loadMenu.js
+++ loadMenu.js
@@ -7,7 +7,6 @@ fetch(url /*, options */)
         content.innerHTML = html.querySelector("#menuplanThisWeek table").innerHTML;
         content.querySelector("button").style.display = "none";
         const day = new Date().getDay() - 1; // 0 is Sunday, minus 1 to index
-        console.log("day is " + day);
         if (day >= 0 && day < 5) {
             /* should be weekday */
             content.querySelectorAll(".td_day")[day].classList.add("today");
